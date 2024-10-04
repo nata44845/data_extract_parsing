7736bbdfeaeafdcd74173b292e62da4c0a327811
nata44845 <krppost@yandex.ru>
2024-10-04 21:16:13 +0700

    homework1

---
+++ seminar1/homework1/task4.py
@@ -0,0 +1,36 @@
+# 3. Сценарий Foursquare
+# 4. Напишите сценарий на языке Python, который предложит пользователю ввести интересующую его категорию (например, кофейни, музеи, парки и т.д.).
+# 5. Используйте API Foursquare для поиска заведений в указанной категории.
+# 6. Получите название заведения, его адрес и рейтинг для каждого из них.
+# 7. Скрипт должен вывести название и адрес и рейтинг каждого заведения в консоль.
+
+import json
+import requests
+
+url = "https://api.foursquare.com/v3/places/search"
+
+headers = {
+    "Authorization": "fsq30crRa1ZQAJVqYBVlkdgrwI+D3NTu+kaZUfGlmBGnpIk=",
+    "accept": "application/json"
+}
+
+city = input("Введите название города: ")
+place = input("Введите категорию заведения (кофейни, музеи, парки и т.д): ")
+
+params = {
+    "near": city,
+    "query": place,
+    "fields": "name,location,rating"
+}
+
+response = requests.get(url, params=params, headers=headers)
+
+if response.status_code == 200:
+    print("Успешный запрос")
+    data = json.loads(response.text)
+    items = data["results"]
+    for item in sorted(items, key = lambda item: item['name']):
+        print(f"{item['name']}, адрес: {item['location']['formatted_address']}, рейтинг {item.get('rating','не определен')}")
+else:
+    print("Запрос завершился ошибкой")
+    print(response.status_code)
