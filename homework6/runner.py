02cacc132178283a691c82084b25b0ae80e307fe
nata44845 <krppost@yandex.ru>
2024-10-23 10:32:56 +0700

    hw6

--- homework6/runner.py
+++ homework6/runner.py
@@ -2,12 +2,14 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.reactor import install_reactor
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
-from photoparser.spiders.unsplash import Book24Spider
+from photoparser.spiders.upsplash import UpsplashSpider
 
 if __name__ == '__main__':
     configure_logging()
     install_reactor("twisted.internet.asyncioreactor.AsyncioSelectorReactor")
+    with open('cats.csv', 'w', encoding='utf-8') as file:
+        print(f"name, categories, image_urls", file=file)
     process = CrawlerProcess(get_project_settings())
-    process.crawl(Book24Spider, query = 'фантастика')
+    process.crawl(UpsplashSpider, query = 'cats')
     process.start()
 
